a3e2b6f2054284bcf4a7f058afdc380d279f6bb1
Gints Polis <polis.gints@gmail.com>
2023-10-07 22:26:39 +0300

    Unused import removed

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use crossterm::{
-    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    event::{self, Event, KeyCode, KeyEventKind, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
